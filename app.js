471ab40a39f281864b74fdd1dbafa1fca51e94d4
Alexa-dlc <144040378+Alexa-dlc@users.noreply.github.com>
2026-09-19 21:15:12 +0000

    ✨ Feat: Enhance app.js Enhance app.js With middleware configuration and error handling comments; update www for debug message; add greet script in package.json; include flores image

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+//Funcion para manehar errores en la aplicacion 
 var createError = require('http-errors');
+//Importar el framework express
 var express = require('express');
+//Imprtar modulos para el manejo de rutas
 var path = require('path');
+//Importar modulos para el manejo de cookies 
 var cookieParser = require('cookie-parser');
+//Importar modulos para el manejo de logs
 var logger = require('morgan');
 
+//Importar las rutas de la aplicacion
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Crea la aplicacion express
 var app = express();
 
-// view engine setup
+//Configura el motor de vistas 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -17,6 +24,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//configura la carpeta de archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
