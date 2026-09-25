baef06420c4359e96d64b254fce6208c2fe873bd
Alexa-dlc <144040378+Alexa-dlc@users.noreply.github.com>
2026-09-25 18:58:37 +0000

    ✨ Feat: Nodemon Update users router response and enchance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1 style="color: blue;">Lista de usuarios no deseados</h1>');
 });
 
 export default router;
